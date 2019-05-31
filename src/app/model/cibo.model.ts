a5bf45cd55d24378cca803c177f1997e343b5a6c
valeriaparascandolo <valeriaparascandolo@libero.it>
2019-05-31 16:31:38 +0200

    nuovo commit

---
+++ src/app/model/cibo.model.ts
@@ -0,0 +1,5 @@
+export class Cibo {
+
+    id: number;
+    nome: string;
+}
---
+++ src/app/model/commento.model.ts
@@ -0,0 +1,10 @@
+export class Commento {
+
+    id: number;
+    testo: string;
+    data: Date;
+    ora: Date;
+    bannato: boolean;
+    idutente: number;
+    idricetta: number;
+}
