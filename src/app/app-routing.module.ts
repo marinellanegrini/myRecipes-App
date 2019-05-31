edbc1763a8b3a9139997f9f43aac71a19c1f9cde
Arianna Tusi <ariannatusi@MacBook-Pro-di-Arianna.local>
2019-05-31 18:58:51 +0200

    Aggiunto preferiti e iniziato tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'preferiti', pathMatch: 'full' },
+  { path: 'preferiti', loadChildren: './pages/preferiti/preferiti.module#PreferitiPageModule'},
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
